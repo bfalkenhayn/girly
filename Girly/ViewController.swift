75f1e84da05607ebe182d85d441d74a9d93bc473
Bridget Falkenhayn <bfalkenhayn01@gmail.com>
2022-04-08 13:43:14 -0400

    Initial Commit

---
+++ Girly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Girly
+//
+//  Created by Bridget Falkenhayn on 4/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
